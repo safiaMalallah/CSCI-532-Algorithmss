78974fea4c56668117d96e4284119505a9168b49
safiaMalallah <safiaAmalallah@gmail.com>
2017-01-20 14:30:42 -0700

    Update changeMoney.java

--- changeMoney.java
+++ changeMoney.java
@@ -8,15 +8,12 @@ public class changeMoney {
 		int c = 0,c11=0,c2=0;
 		final Scanner STDIN_SCANNER = new Scanner(System.in);	
 		//reading money
-		/*
 		System.out.println("How much dollars you owed? ");
 		c11 = STDIN_SCANNER.nextInt(); 
 		System.out.println("How much cents you owed? ");
 		c2 = STDIN_SCANNER.nextInt(); 
-		*/
 		//convert all money to cents
 		c= c2+ (c11*100);
-		c=44;
 		System.out.print("with "+c+" cents ");
 		int c25 = 0, c10 = 0, c5 = 0, c1 = 0;
 		//Gets 25c coins
